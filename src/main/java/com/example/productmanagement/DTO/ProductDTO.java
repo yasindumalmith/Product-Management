29618a3a92f976ec641e16fbc6fcbf6b2a4cf3e2
Yasindu <yasindumalmith058@gmail.com>
2025-10-25 16:53:34 +0530

    Create ProductDTO file

--- src/main/java/com/example/productmanagement/DTO/ProductDTO.java
+++ src/main/java/com/example/productmanagement/DTO/ProductDTO.java
@@ -1,4 +1,24 @@
 package com.example.productmanagement.DTO;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductDTO {
+    @NotBlank(message = "Product Name is Requird")
+    private String productName;
+    @NotBlank(message = "Category is Requird ")
+    private String category;
+    @NotNull(message = "Price must requird")
+    @Min(value = 1, message = "Value must be at least 1")
+    private Double price;
+    @NotNull(message = "Quantity must requird")
+    @Min(value = 1, message = "Value must be at least 1")
+    private Integer quantity;
 }
