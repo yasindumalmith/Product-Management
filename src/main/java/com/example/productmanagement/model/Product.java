1da7b0851fe3d0eb8288a66a5f414d152fb04045
Yasindu <yasindumalmith058@gmail.com>
2025-10-25 16:30:16 +0530

    Create Product class

--- src/main/java/com/example/productmanagement/model/Product.java
+++ src/main/java/com/example/productmanagement/model/Product.java
@@ -1,4 +1,26 @@
 package com.example.productmanagement.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Table(name = "product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "product_name",nullable = false)
+    private String productName;
+    @Column(name = "category", nullable = false)
+    private String category;
+    @Column(name = "price",nullable = false)
+    private Double price;
+    @Column(name = "quantity",nullable = false)
+    private Integer quantity;
+
 }
